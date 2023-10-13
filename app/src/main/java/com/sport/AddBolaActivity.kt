5571eca694349a455555f5992a5ba5504cda13f4
stwnmhmdluthfi <mhmdlutfi969@gmail.com>
2023-10-13 09:11:17 +0700

    Penambahan File

---
+++ app/src/main/java/com/sport/AddBolaActivity.kt
@@ -0,0 +1,11 @@
+package com.sport
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddBolaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_bola)
+    }
+}
